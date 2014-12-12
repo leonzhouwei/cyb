985e3275f5a90f514bb92ae0ebdffe2cc10131ae
wei.zhou <wei.zhou@autonavi.com>
2014-12-12 14:54:50 +0800

    set the project hierarchy

---
+++ src/main/java/com/example/cyb/webapi/controller/BaseRoute.java
@@ -0,0 +1,14 @@
+package com.example.cyb.webapi.controller;
+
+public final class BaseRoute {
+	
+	private BaseRoute() {
+	}
+
+	public static final String ROOT = "/";
+	
+	public static final String ADMIN = ROOT + "admin";
+	public static final String DELEARS = ROOT + "dealers";
+	public static final String GUI = ROOT + "ui";
+	
+}
